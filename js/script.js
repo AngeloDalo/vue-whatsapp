ad90eda54d144b3b052ce943ac821d8cd29260ef
AngeloDalo <angelo251158@gmail.com>
2021-12-22 12:15:58 +0100

    inizio m.4

--- js/script.js
+++ js/script.js
@@ -16,6 +16,12 @@
   Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo
  */
 
+/**
+ * Milestone 4
+  Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+ */
+
+
  const app = new Vue(
     {
         el: '#app',
