e2486a1501e7544e3400e42760b08478c8627ce8
AngeloDalo <angelo251158@gmail.com>
2021-12-26 11:26:23 +0100

    aeroplano

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -30,6 +30,11 @@
  * evitare che l'utente possa inviare un messaggio vuoto o composto solamente da spazi
  */
 
+/**
+ * BONUS 2
+ * A) cambiare icona in basso a destra (a fianco all'input per scrivere un nuovo messaggio) finché l'utente sta scrivendo: di default si visualizza l'icona del microfono, quando l'input non è vuoto si visualizza l'icona dell'aeroplano. Quando il messaggio è stato inviato e l'input si svuota, si torna a visualizzare il microfono. B) inviare quindi il messaggio anche cliccando sull'icona dell'aeroplano
+ */
+
 
  const app = new Vue(
     {
