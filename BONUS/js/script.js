bccdecf056deee34175534092f6771406ea48ef6
AngeloDalo <angelo251158@gmail.com>
2021-12-27 12:02:24 +0100

    traccia bonus 7

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -55,6 +55,11 @@
  * sotto al nome del contatto nella parte in alto a destra, cambiare l'indicazione dello stato: visualizzare il testo "sta scrivendo..." nel timeout in cui il pc risponde, poi mantenere la scritta "online" per un paio di secondi e infine visualizzare "ultimo accesso alle xx:yy" con l'orario corretto
  */
 
+/**
+ * BONUS 7
+ * dare la possibilità all'utente di cancellare tutti i messaggi di un contatto o di cancellare l'intera chat con tutti i suoi dati: cliccando sull'icona con i tre pallini in alto a destra, si apre un dropdown menu in cui sono presenti le voci "Elimina messaggi" ed "Elimina chat"; cliccando su di essi si cancellano rispettivamente tutti i messaggi di quel contatto (quindi rimane la conversazione vuota) oppure l'intera chat comprensiva di tutti i dati del contatto oltre che tutti i suoi messaggi (quindi sparisce il contatto anche dalla lista di sinistra)
+ */
+
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
@@ -291,7 +296,6 @@ const app = new Vue(
 
           //eliminazione del messaggio selezionato
           delateMessage: function (chat, numero) {
-            console.log(chat, numero);
             this.contacts[numero].messages.splice(chat, 1);
           }
         }
